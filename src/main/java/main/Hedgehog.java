c6d03737976d71095d5645b2f7bc3d5de961d862
jomppe123 <jomppe123@users.noreply.github.com>
2024-01-19 12:39:26 +0200

    Add files via upload

---
+++ src/main/java/main/Hedgehog.java
@@ -0,0 +1,34 @@
+package main;
+
+public class Hedgehog {
+    private String name;
+    private int age;
+
+    public Hedgehog(){
+        name = "Pikseli";
+        age = 5;
+
+    }
+
+    public Hedgehog(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void speak(String print){
+        System.out.println(name+": " + print);
+    }
+
+    public void speakEmpty(){
+        System.out.println("Olen "+name+" ja ikäni on " + age + " vuotta, mutta antaisitko silti syötteen?");
+    }
+
+    public void run(int rounds){
+        for (int i = 0; i < rounds; i++){
+            System.out.println(name+" juoksee kovaa vauhtia!");
+
+        }
+
+    }
+
+}
